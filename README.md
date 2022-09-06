46340d810e7fc695c54ce317d7eab6b6f64aa53e
RREE <rolf.ebert.gcc@gmx.de>
2022-09-06 09:59:28 +0200

    further examples, voltmeter moved to basic

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AVRAda_Examples
 Sample applications in Ada for AVR microcontrollers
+
+This is a fake crate. The actual examples are in the subdirectories.
--- avrada_examples.adb
+++ avrada_examples.adb
@@ -1,6 +1,6 @@
 with Ada.Text_IO;
 procedure AVRAda_Examples is
-   use Ada.Text_Io;
+   use Ada.Text_IO;
 begin
    Put_Line ("This is just a placeholder to have a non-empty project.");
    Put_Line ("The subdirectories are separate crates. See there for info on how");
