57ee047abc6cf5709be38d2a5330db85bfb4b396
RREE <rolf.ebert.gcc@gmx.de>
2022-10-04 19:37:40 +0200

    clean up of all alire.toml files

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # AVRAda_Examples
 Sample applications in Ada for AVR microcontrollers
 
-This is a fake crate. The actual examples are in the subdirectories.
+This is a collection crate. The toplevel program dos not do anything
+useful. It servers as a container for the actual examples in the
+subdirectories.
+
+The best starting point is `basic_examples`.
--- avrada_examples.adb
+++ avrada_examples.adb
@@ -2,7 +2,10 @@ with Ada.Text_IO;
 procedure AVRAda_Examples is
    use Ada.Text_IO;
 begin
+   New_Line;
    Put_Line ("This is just a placeholder to have a non-empty project.");
    Put_Line ("The subdirectories are separate crates. See there for info on how");
    Put_Line ("to use Alire for building programs on AVR microcontrollers.");
+   New_Line;
+   Put_Line ("The best starting point is in 'basic_examples'");
 end AVRAda_Examples;
